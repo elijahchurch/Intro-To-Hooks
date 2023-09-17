3a70ba40b7be2dc8aeae80e06754fd81509fcec3
Jason E. Church <elijahchurch@gmail.com>
2023-09-17 15:39:37 -0700

    Add second useState hook in Counter function

--- src/Counter.js
+++ src/Counter.js
@@ -2,10 +2,13 @@ import React, { useState} from "react";
 
 function Counter() {
     const [counter, setCounter] = useState(0);
+    const [hidden, setHidden] = useState(false);
+
     return(
         <React.Fragment>
-            <h1>{counter}</h1>
+            {hidden ? <h1>CountHidden</h1> : <h1>{counter}</h1>}
             <button onClick={() => setCounter(counter +1)}>Count!</button>
+            <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
         </React.Fragment>
     )
 }
